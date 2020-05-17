9992c457fe0a5ea559b6d37bead241685eeb2610
Jessica McCabe <mccabejessica88@gmail.com>
2020-05-17 17:03:41 +0100

    Add Users

--- src/views/pois.ts
+++ src/views/pois.ts
@@ -6,9 +6,11 @@ import { PoiService } from '../services/poi-service';
 export class Pois {
   pois: Poi[] = [];
   category = ['Castle', 'Forest'];
+  total = 0;
 
   constructor(private ds: PoiService){
     this.pois = ds.pois;
     this.category = ds.category;
+    this.total = ds.total;
   }
 }
