788304b04e9c4c3bdcc1df442e1d6158eee31074
Jessica McCabe <mccabejessica88@gmail.com>
2020-06-05 10:37:53 +0100

    Upload Image

--- src/appImage.ts
+++
@@ -1,7 +0,0 @@
-import { autoinject } from 'aurelia-framework';
-import { ImageStore } from './services/image-store';
-
-@autoinject
-export class App {
-  constructor(private imageStore: ImageStore) {}
-}
